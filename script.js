aa36e29db73b47befa0ea66e4c630ab4153c954c
Nikesupermega <leonardo.bortolin@outlook.com>
2026-01-04 13:24:08 +0100

    Refactor Firebase initialization and game logic

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function entraStanza() {
 
   assegnaHost();
   ascoltaNumeri();
-  controllavittoria();
+  controllaVittoria();
 }
 
 // HOST AUTOMATICO
@@ -154,6 +154,7 @@ function resetPartita() {
   baseRef.child("numeriUsciti").set([]);
   baseRef.child("vittoria").remove();
 }
+
 
 
 
